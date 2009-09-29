53c05b7382d9c3b6126a5ce7c1f7d75501756069
Stephen Tung <stktung@yahoo.com>
2009-09-29 14:51:36 -0400

    synced again

--- PullFromJP.ps1
+++
@@ -1,4 +0,0 @@
-git checkout master
-git pull jp master
-git merge development
-git checkout development
--- commit.ps1
+++
@@ -1,9 +0,0 @@
-param($commit_message)
-
-if (!$commit_message) {
-   'usage: commit.ps1 <commit message>'
-   exit 1
-}
-
-git add -A
-git commit -m $commit_message
