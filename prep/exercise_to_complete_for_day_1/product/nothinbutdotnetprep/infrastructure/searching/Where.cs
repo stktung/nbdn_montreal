e950a7e59363d00ebb44b119ece6ce89f9c7a55c
Chris Manley <cam@dacha.ca>
2009-09-29 10:41:13 -0400

    Starting to deal with Comparable Properties

--- prep/exercise_to_complete_for_day_1/product/nothinbutdotnetprep/infrastructure/searching/Where.cs
+++ prep/exercise_to_complete_for_day_1/product/nothinbutdotnetprep/infrastructure/searching/Where.cs
@@ -9,9 +9,29 @@ namespace nothinbutdotnetprep.infrastructure.searching
             return new DefaultCriteriaFactory<ItemToFilter, Property>(property_accessor);
         }
 
-        static public void has_an<Property>(Func<ItemToFilter, Property> property_accessor) where Property : IComparable<Property>
+        static public ComparableCriteriaFactory<ItemToFilter, Property> has_an<Property>(Func<ItemToFilter, Property> property_accessor) where Property : IComparable<Property>
+        {
+            return new ComparableCriteriaFactory<ItemToFilter, Property>(property_accessor);
+        }
+    }
+
+    public class ComparableCriteriaFactory<ItemToFilter, Property> where Property : IComparable<Property>
+    {
+        Func<ItemToFilter, Property> property_accessor;
+
+        public ComparableCriteriaFactory(Func<ItemToFilter, Property> accessor)
+        {
+            property_accessor = accessor;
+        }
+
+        public Criteria<ItemToFilter> equal_to(Property property_to_compare) {
+            return new AnonymousCriteria<ItemToFilter>(item => property_accessor(item) == property_to_compare);
+        }
+
+        public Criteria<ItemToFilter> equal_to_any(params Property[] values)
         {
             throw new NotImplementedException();
         }
     }
+
 }
