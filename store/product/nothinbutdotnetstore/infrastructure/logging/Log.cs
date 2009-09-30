55959ef042aa43042a4bbe0e4252c03c3cc12ac4
unknown <Administrator@.claritysystems.com>
2009-09-30 15:53:56 -0400

    Implement Log bound_to method

--- store/product/nothinbutdotnetstore/infrastructure/logging/Log.cs
+++ store/product/nothinbutdotnetstore/infrastructure/logging/Log.cs
@@ -4,14 +4,16 @@ namespace nothinbutdotnetstore.infrastructure.logging
 {
     public class Log
     {
+        private static LogFactory _log_factory;
+
         static public Logger bound_to(Type type_that_requires_logging)
         {
-            throw new NotImplementedException();
+            return _log_factory.create_logger_for(type_that_requires_logging);
         }
 
         static public void initialize_with(LogFactory log_factory)
         {
-            throw new NotImplementedException();
+            _log_factory = log_factory;
         }
     }
 }
