592af3a43839594f765e6d6694d4f4fe648a07ba
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2009-09-30 15:10:56 -0600

    Finishing logger

--- store/product/nothinbutdotnetstore/infrastructure/logging/LogFactory.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace nothinbutdotnetstore.infrastructure.logging
-{
-    public interface LogFactory
-    {
-        Logger create_logger_for(Type type_that_requires_logging_services);
-    }
-}
