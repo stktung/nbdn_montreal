c9997a6c8f1057f37a081ed5d7b03f5b1a027386
Chris Manley <cam@dacha.ca>
2009-10-01 12:36:17 -0400

    Adding specification for ViewMainDepartment

--- store/product/nothinbutdotnetstore.tests/web/ViewMainDepartmentSpecificationSpecs.cs
+++ store/product/nothinbutdotnetstore.tests/web/ViewMainDepartmentSpecificationSpecs.cs
@@ -1,43 +1,38 @@
- using System;
- using developwithpassion.bdd.contexts;
- using developwithpassion.bdd.harnesses.mbunit;
- using developwithpassion.bdddoc.core;
- using nothinbutdotnetstore.infrastructure;
- using nothinbutdotnetstore.web.core;
+using developwithpassion.bdd.contexts;
+using developwithpassion.bdd.harnesses.mbunit;
+using developwithpassion.bdddoc.core;
+using nothinbutdotnetstore.infrastructure;
+using nothinbutdotnetstore.web.core;
 
 namespace nothinbutdotnetstore.tests.web
- {
+{
     public class ViewMainDepartmentSpecificationSpecs
-     {
-         public abstract class concern : observations_for_a_sut_with_a_contract<Specification<ApplicationRequest>,
-                                             ViewMainDepartmentSpecification>
-         {
-        
-         }
+    {
+        public abstract class concern : observations_for_a_sut_with_a_contract<Specification<ApplicationRequest>,
+                                            ViewMainDepartmentSpecification> {}
 
-         [Concern(typeof(ViewMainDepartmentSpecification))]
-         public class when_specification_is_given_an_application_request_that_satisfies : concern
-         {
-             context c = () =>
-             {
-                 request = an<ApplicationRequest>();
-                 request.CommandName = "ViewMainDepartments";
-             };
+        [Concern(typeof (ViewMainDepartmentSpecification))]
+        public class when_command_name_on_request_matches_what_is_expected_by_specification : concern
+        {
+            context c = () =>
+            {
+                request = an<ApplicationRequest>();
+                request.CommandName = "ViewMainDepartments";
+            };
 
-             because b = () =>
-             {
-                 result = sut.is_satisfied_by(request);
-             };
+            because b = () =>
+            {
+                result = sut.is_satisfied_by(request);
+            };
 
-        
-             it should_return_true = () =>
-             {
-                 result.should_be_true();
-            
-             };
 
-             static ApplicationRequest request;
-             static bool result;
-         }
-     }
- }
+            it should_return_true = () =>
+            {
+                result.should_be_true();
+            };
+
+            static ApplicationRequest request;
+            static bool result;
+        }
+    }
+}
