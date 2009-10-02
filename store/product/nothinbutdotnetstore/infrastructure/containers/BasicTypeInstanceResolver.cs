f6874d7be3907afd489930c5ba35924e97d08c8c
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2009-10-02 10:20:08 -0600

    Adding a BasicTypeInstanceResolver

---
+++ store/product/nothinbutdotnetstore/infrastructure/containers/BasicTypeInstanceResolver.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace nothinbutdotnetstore.infrastructure.containers
+{
+    public class BasicTypeInstanceResolver : TypeInstanceResolver
+    {
+        public object resolve()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
