c2e64d8c5990038c728ca1a8443ae01841b58130
Chris Manley <cam@dacha.ca>
2009-10-02 16:13:09 -0400

    More shifting of the startpu methods

--- store/product/nothinbutdotnetstore/tasks/startup/Startup.cs
+++ store/product/nothinbutdotnetstore/tasks/startup/Startup.cs
@@ -11,47 +11,66 @@ namespace nothinbutdotnetstore.tasks.startup
 {
     public class Startup
     {
+        
         static public void go()
         {
-            configure_front_controller();
-            configure_service_layer();
             configure_commands();
+            configure_front_controller();
+            configure_request_factory();
+            configure_service_layer();
+
         }
 
-        static private void configure_front_controller() {
-            var resolvers = new ResolverFactory();
-            var config = new Dictionary<Type, TypeInstanceResolver>();
+        static void configure_request_factory()
+        {
+            var request_factory_resolver = resolvers.create(() => new BasicRequestFactory());
+            config.Add(typeof(RequestFactory), request_factory_resolver);
+        }
+
+        static void configure_service_layer()
+        {
+            catalog_tasks = new StubCatalogTasks();
+            view_model_to_views = new Dictionary<Type, Type>();
+            response_engine = new BasicResponseEngine(new BasicViewRegistry(view_model_to_views));
+        }
+
+        static private void configure_front_controller()
+        {
             Container startup_container = new BasicContainer(config);
             IOC.initialize_with(startup_container);
 
-            var commands = configure_commands();
-
-            var front_controller_resolver = resolvers.create(() => new BasicFrontController(new BasicCommandRegistry(commands)));
-            var request_factory_resolver = resolvers.create(() => new BasicRequestFactory());
-
-            config.Add(typeof (RequestFactory), request_factory_resolver);
-            config.Add(typeof (FrontController), front_controller_resolver);
+            var front_controller_resolver = resolvers.create(() => new BasicFrontController(new BasicCommandRegistry(list_of_commands)));
+            config.Add(typeof(FrontController), front_controller_resolver);
         }
 
-        static List<ApplicationRequestCommand> configure_commands()
+        static void configure_commands()
         {
-            var catalog_tasks = new StubCatalogTasks();
-            var view_model_to_views = new Dictionary<Type, Type>();
-            var response_engine = new BasicResponseEngine(new BasicViewRegistry(view_model_to_views));
 
-            return new List<ApplicationRequestCommand>
-                   {
+
+            list_of_commands.Add(
                        new BasicApplicationRequestCommand(
                            Request.has_a_url_that_contains_the_command<ViewMainDepartments>(),
-                           new ViewMainDepartments(catalog_tasks, response_engine)),
-                       new BasicApplicationRequestCommand(Request.compound_specification(
-                                                              Request.has_a_url_that_contains_the_command<ViewProductBrowser>(),
-                                                              new DepartmentHasProducts(catalog_tasks)), new ViewProductBrowser(catalog_tasks, response_engine)),
-                       new BasicApplicationRequestCommand(
-                           Request.has_a_url_that_contains_the_command<ViewSubDepartments>(),
-                           new ViewSubDepartments(catalog_tasks, response_engine))
-                   };
+                           new ViewMainDepartments(catalog_tasks, response_engine)));
+
+            list_of_commands.Add(
+                new BasicApplicationRequestCommand(Request.compound_specification(
+                                                       Request.has_a_url_that_contains_the_command<ViewProductBrowser>(),
+                                                       new DepartmentHasProducts(catalog_tasks)), new ViewProductBrowser(catalog_tasks, response_engine)));
+
+            list_of_commands.Add(
+                new BasicApplicationRequestCommand(
+                    Request.has_a_url_that_contains_the_command<ViewSubDepartments>(),
+                    new ViewSubDepartments(catalog_tasks, response_engine)));
+
         }
+
+        static ResolverFactory resolvers = new ResolverFactory();
+        static IDictionary<Type, TypeInstanceResolver> config = new Dictionary<Type, TypeInstanceResolver>();
+        static List<ApplicationRequestCommand> list_of_commands;
+        static CatalogTasks catalog_tasks;
+        static IDictionary<Type, Type> view_model_to_views;
+        static ResponseEngine response_engine;
+
 
         public class ResolverFactory
         {
@@ -61,3 +80,4 @@ namespace nothinbutdotnetstore.tasks.startup
             }
         }
     }
+}
