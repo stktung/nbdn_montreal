164b27e1fd2c98a366fdf608c8895b92118bd69a
Chris Manley <cam@dacha.ca>
2009-10-02 13:12:32 -0400

    First go at Startup

--- store/product/nothinbutdotnetstore/tasks/startup/Startup.cs
+++ store/product/nothinbutdotnetstore/tasks/startup/Startup.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Collections.Generic;
+using developwithpassion.commons.core.infrastructure.containers;
+using nothinbutdotnetstore.infrastructure.containers;
+using nothinbutdotnetstore.web.core;
 
 namespace nothinbutdotnetstore.tasks.startup
 {
@@ -6,7 +10,29 @@ namespace nothinbutdotnetstore.tasks.startup
     {
         static public void go()
         {
-            throw new NotImplementedException();
+
+            ResolverFactory resolvers = new ResolverFactory();
+
+            IDictionary<Type, TypeInstanceResolver> config = new Dictionary<Type, TypeInstanceResolver>();
+
+            IEnumerable<ApplicationRequestCommand> commands = new List<ApplicationRequestCommand>();
+
+            TypeInstanceResolver front_controller_resolver = resolvers.create(() =>
+                                                             new BasicFrontController(new BasicCommandRegistry(commands)));
+            TypeInstanceResolver request_factory_resolver = resolvers.create(() => new BasicRequestFactory());
+           
+            config.Add(typeof(RequestFactory), request_factory_resolver);
+            config.Add(typeof(FrontController), front_controller_resolver); 
+            
+            Container startup_container = new BasicContainer(config);
+            IOC.initialize_with(startup_container);
+        }
+    }
+
+    public class ResolverFactory
+    {
+        public BasicTypeInstanceResolver create(Func<object> factory) { 
+            return new BasicTypeInstanceResolver(factory);
         }
     }
 }
