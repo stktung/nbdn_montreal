8deea147cb9dfc443cbf9e5aad68ffd1f9b77233
Stephen Tung <stktung@yahoo.com>
2009-10-02 10:39:57 -0400

    Implemented instance_of() of an IOC

--- store/product/nothinbutdotnetstore.tests/infrastructure/BasicContainerSpecs.cs
+++ store/product/nothinbutdotnetstore.tests/infrastructure/BasicContainerSpecs.cs
@@ -1,39 +1,50 @@
- using developwithpassion.bdd.contexts;
- using developwithpassion.bdd.harnesses.mbunit;
- using developwithpassion.bdddoc.core;
- using developwithpassion.commons.core.infrastructure.containers;
- using nothinbutdotnetstore.infrastructure.containers;
+using System;
+using System.Collections.Generic;
+using developwithpassion.bdd.contexts;
+using developwithpassion.bdd.harnesses.mbunit;
+using developwithpassion.bdddoc.core;
+using developwithpassion.commons.core.infrastructure.containers;
+using nothinbutdotnetstore.infrastructure.containers;
 
 namespace nothinbutdotnetstore.tests.infrastructure
- {   
-     public class BasicContainerSpecs
-     {
-         public abstract class concern : observations_for_a_sut_with_a_contract<Container,
-                                             BasicContainer>
-         {
-        
-         }
+{
+    public class BasicContainerSpecs
+    {
+        public abstract class concern : observations_for_a_sut_with_a_contract<Container,
+                                            BasicContainer> {}
 
-         [Concern(typeof(BasicContainer))]
-         public class when_resolving_an_implementation_of_an_dependency : concern
-         {
-             context c = () =>
-             {
-            
-             };
+        [Concern(typeof (BasicContainer))]
+        public class when_resolving_an_implementation_of_an_dependency : concern
+        {
+            context c = () =>
+            {
+                types = new Dictionary<Type, object>();
 
-             because b = () =>
-             {
-        
-             };
+                implementation = new MyImplementation();
 
-        
-             it should = () =>
-             {
-                 
-            
-            
-             };
-         }
-     }
- }
+                types.Add(typeof(MyInterface), implementation);
+                
+                provide_a_basic_sut_constructor_argument<IDictionary<Type, object>>(types);
+            };
+
+            because b = () =>
+            {
+                result = sut.instance_of<MyInterface>();
+            };
+
+
+            it should_should_give_back_an_implementation_of_the_dependency = () => 
+            {
+                result.should_be_equal_to(implementation);
+            };
+
+            static MyInterface result;
+            static Dictionary<Type, object> types;
+            static object implementation;
+        }
+    }
+
+    class MyImplementation : MyInterface {}
+
+    interface MyInterface {}
+}
--- store/product/nothinbutdotnetstore/infrastructure/containers/BasicContainer.cs
+++ store/product/nothinbutdotnetstore/infrastructure/containers/BasicContainer.cs
@@ -6,19 +6,33 @@ namespace nothinbutdotnetstore.infrastructure.containers
 {
     public class BasicContainer : Container
     {
+        IDictionary<Type, object> types;
+
+        public BasicContainer(IDictionary<Type, object> types)
+        {
+            this.types = types;
+        }
+
         public Dependency instance_of<Dependency>()
         {
-            throw new NotImplementedException();
+            return (Dependency) instance_of(typeof (Dependency));
         }
 
         public object instance_of(Type dependency_type)
         {
-            throw new NotImplementedException();
+            object obj;
+            if (types.TryGetValue(dependency_type, out obj))
+            {
+                return obj;
+            }
+
+            return null;
         }
 
         public IEnumerable<DependencyType> all_instances_of<DependencyType>()
         {
             throw new NotImplementedException();
         }
+
     }
 }
