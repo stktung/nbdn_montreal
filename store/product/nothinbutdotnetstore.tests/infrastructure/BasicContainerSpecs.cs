eb654dc8580d0138c02fd0715dda372a6e087c6e
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2009-10-02 09:31:00 -0600

    Adding more behaviour to the BasicContainer

--- store/product/nothinbutdotnetstore.tests/infrastructure/BasicContainerSpecs.cs
+++ store/product/nothinbutdotnetstore.tests/infrastructure/BasicContainerSpecs.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using developwithpassion.bdd.contexts;
 using developwithpassion.bdd.harnesses.mbunit;
 using developwithpassion.bdddoc.core;
@@ -11,30 +13,28 @@ namespace nothinbutdotnetstore.tests.infrastructure
     public class BasicContainerSpecs
     {
         public abstract class concern : observations_for_a_sut_with_a_contract<Container,
-                                            BasicContainer> {
-                                                context c = () =>
-                                                {
-                                                    types = new Dictionary<Type, Type>();
-                                                    types.Add(typeof(MyInterface), typeof(MyImplementation));
-                                                    provide_a_basic_sut_constructor_argument(types);
+                                            BasicContainer>
+        {
+            context c = () =>
+            {
+                types = new Dictionary<Type, Type>();
+                types.Add(typeof (MyInterface), typeof (MyImplementation));
+                provide_a_basic_sut_constructor_argument(types);
+            };
 
-                                                };
-
-                                                static protected Dictionary<Type, Type> types;
-           
-                                            }
+            static protected IDictionary<Type, Type> types;
+        }
 
         [Concern(typeof (BasicContainer))]
         public class when_resolving_an_implementation_of_an_dependency_and_the_dependency_has_been_configured : concern
         {
-            
             because b = () =>
             {
                 result = sut.instance_of<MyInterface>();
             };
 
 
-            it should_should_give_back_an_implementation_of_the_dependency = () => 
+            it should_should_give_back_an_implementation_of_the_dependency = () =>
             {
                 result.should_be_an_instance_of<MyImplementation>();
             };
@@ -47,22 +47,50 @@ namespace nothinbutdotnetstore.tests.infrastructure
         {
             because b = () =>
             {
-                doing(() =>sut.instance_of<MyImplementation>());
+                doing(() => sut.instance_of<MyInterface>());
             };
 
 
-            it should_throw_a_dependency_not_registered_exception = () => 
+            it should_throw_a_dependency_not_registered_exception = () =>
             {
                 exception_thrown_by_the_sut.should_be_an_instance_of<DependencyNotRegisteredException>()
-                                           .type_that_could_not_be_resolved.should_be_equal_to(typeof(MyImplementation));
+                    .type_that_could_not_be_resolved.should_be_equal_to(typeof (MyImplementation));
             };
+        }
+
+        [Concern(typeof (BasicContainer))]
+        public class when_resovling_an_implementation_of_an_dependency_with_dependencies : concern
+        {
+            because b = () =>
+            {
+                result = sut.instance_of<MyInterfaceWithDependencies>();
+            };
+
+
+            it should_return_the_item_with_all_of_its_dependencies = () =>
+            {
+                result.should_not_be_an_instance_of<MyInterfaceWithDependenciesImpl>();
+            };
+
+            static MyInterfaceWithDependencies result;
         }
     }
 
-    class MyImplementation : MyInterface {
-    
+public interface  MyInterfaceWithDependencies {}
 
+    public class MyInterfaceWithDependenciesImpl : MyInterfaceWithDependencies {
+        IDbConnection first_dependency;
+        ConnectionStringSettings second_dependency;
+        IList<int> third_dependency;
+        public MyInterfaceWithDependenciesImpl(IDbConnection first_dependency, ConnectionStringSettings second_dependency, IList<int> third_dependency)
+        {
+            this.first_dependency = first_dependency;
+            this.second_dependency = second_dependency;
+            this.third_dependency = third_dependency;
+        }
     }
+
+    class MyImplementation : MyInterface {}
 
     interface MyInterface {}
 }
